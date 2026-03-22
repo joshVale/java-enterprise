18292aa37fcbc3764e49337f36be184a6a667ad4
1Zholdoshbek <zholdoshbekyashiev@gmail.com>
2026-03-22 17:14:27 +0600

    homework-6

---
+++ src/Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+    int age;
+    String name;
+    String surname;
+    String position;
+    String phoneNumber;
+    String email;
+    public Employee(int age,String name,String surname,String phoneNumber,String position,String email){
+        this.age = age;
+        this.name = name;
+        this.surname = surname;
+        this.position = position;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
