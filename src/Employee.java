f3cf478115b128d2c53aba7ca0e4fc0687eaf94d
1Zholdoshbek <zholdoshbekyashiev@gmail.com>
2026-03-23 17:42:25 +0600

    lesson-6 reviewed

--- src/Employee.java
+++ src/Employee.java
@@ -1,11 +1,11 @@
 public class Employee {
-    int age;
-    String name;
-    String surname;
-    String position;
-    String phoneNumber;
-    String email;
-    public Employee(int age,String name,String surname,String phoneNumber,String position,String email){
+    private int age;
+    private String name;
+    private String surname;
+    private String position;
+    private String phoneNumber;
+    private String email;
+    public Employee(int age, String name, String surname, String phoneNumber, String position, String email){
         this.age = age;
         this.name = name;
         this.surname = surname;
